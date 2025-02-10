9efc109991570a4a4b4d1490968d412a4369cf50
guiguik12 <guigui_pvh@hotmail.com>
2025-02-10 16:46:13 -0400

    Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,20 @@
+public class Cliente {
+    private int id;
+    private String nome;
+
+    public Cliente(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public String toString() {
+        return "Cliente [ID=" + id + ", Nome=" + nome + "]";
+    }
+}
